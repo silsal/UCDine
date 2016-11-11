047768d241c8e82ee55ba01a4aa48a80174b4d3d
SilSal <salonisilvia@gmail.com>
2016-11-11 01:15:08 +0000

    last changes

---
+++ app/src/main/java/com/example/saorla/ucdfood/printEventFields.java
@@ -0,0 +1,26 @@
+package com.example.saorla.ucdfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Cometa on 09/11/2016.
+ */
+public class printEventFields extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(CreateEvent.EVENT_TITLE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
