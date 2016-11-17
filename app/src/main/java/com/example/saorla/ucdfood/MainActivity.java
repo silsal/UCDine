d0d83266d24e618110a7ce9887ecc52021bf850f
SilSal <salonisilvia@gmail.com>
2016-11-17 17:05:11 +0000

    new working app

--- app/src/main/java/com/example/saorla/ucdfood/MainActivity.java
+++ app/src/main/java/com/example/saorla/ucdfood/MainActivity.java
@@ -23,30 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View v){
         if(v.getId()== R.id.Blogin){
-//
-//            EditText a = (EditText)findViewById(R.id.TFusername);
-//            String str = a.getText().toString();
-//            EditText b = (EditText)findViewById(R.id.TFpassword);
-//            String pass = b.getText().toString();
-//
-//            String password = helper.searchPass(str);
-//            String id = helper.searchId(str);
-//
-//            //Store Value into shared preference!!
-//            storeIdToSharedPreference(id);
-//            //getIdfromSharedPrefernece();
 
-            //displayUserId();
+            EditText a = (EditText)findViewById(R.id.TFusername);
+            String str = a.getText().toString();
+            EditText b = (EditText)findViewById(R.id.TFpassword);
+            String pass = b.getText().toString();
+
+            String password = helper.searchPass(str);
+            String id = helper.searchId(str);
+
+            //Store Value into shared preference!!
+            storeIdToSharedPreference(id);
+//            getIdfromSharedPrefernece();
+
+//            displayUserId();
 
 
-//            if(pass.equals(password)){
+            if(pass.equals(password)){
             Intent i = new Intent(MainActivity.this, Display.class);
-//                i.putExtra("Username",str);
+                i.putExtra("Username",str);
             startActivity(i);
-//            }else{
-//                Toast temp = Toast.makeText(MainActivity.this, "Username and password don't match!",Toast.LENGTH_SHORT);
-//                temp.show();
-//            }
+            }else{
+                Toast temp = Toast.makeText(MainActivity.this, "Username and password don't match!",Toast.LENGTH_SHORT);
+                temp.show();
+            }
 
 
         }
