2170254d3b9bf0a8739bb31973ef21602855c787
Silsal <salonisilvia@gmail.com>
2016-12-01 21:24:01 +0000

    the function

--- app/src/main/java/com/example/saorla/ucdfood/Display.java
+++ app/src/main/java/com/example/saorla/ucdfood/Display.java
@@ -3,11 +3,18 @@ package com.example.saorla.ucdfood;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 /**
  * Created by user on 09/11/2016.
@@ -19,6 +26,24 @@ public class Display extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
+    }
+
+
+        public void flash(View v) {
+            final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
+            animation.setDuration(300); // duration - half a second
+            animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+            animation.setRepeatCount(5); // Repeat animation infinitely
+            animation.setRepeatMode(Animation.REVERSE);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.sheep);
+
+            if(v.getId()==R.id.BSheep){
+                mp.start();
+                v.startAnimation(animation);
+            }
+
+        }
 
 //        String username = getIntent().getStringExtra("Username");
 //
@@ -29,7 +54,7 @@ public class Display extends AppCompatActivity {
 
 
 
-    }
+
 //    public void displayUserId (){
 //        String userid = getIdfromSharedPrefernece();
 //        TextView textView = (TextView)findViewById(R.id.TVdisplayid);
@@ -62,5 +87,8 @@ public class Display extends AppCompatActivity {
         Intent i = new Intent(this, EventList.class);
         startActivity(i);
     }
+
+
+
 
 }
